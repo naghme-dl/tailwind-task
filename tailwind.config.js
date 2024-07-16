709d54666e3c54fd189861d78301928f8b0db77f
naghme dalvand <129004469+naghme-dl@users.noreply.github.com>
2024-07-16 22:20:07 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: { fontFamily: { sans: ["vazir"] } },
+  },
+  plugins: [],
+};
